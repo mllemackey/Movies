009ac9ca7184c0c0aedeaadf13b7a97fa34f76e7
Vivify Academy <mllemackey@gmail.com>
2017-09-01 18:17:06 +0200

    Zadatak 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { MoviesComponent } from './components/movies/movies.component';
-import {MovieService} from './shared/services/movie.service';
+import { MovieService } from './shared/services/movie.service';
+import { SearchComponent } from './components/search/search.component';
+import {SearchModule} from './shared/search.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
-    MoviesComponent
+    MoviesComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    SearchModule
   ],
   providers: [MovieService],
   bootstrap: [AppComponent]
